47a222b5c6438555884c58d18e0262a8a9041405
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-11 18:18:04 +1000

    added copde to the header element

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-// this should be a photot of me but ill add it later
-import ME from '../../assets/Metro-Boomin-Spider-Man.webp'
+import ME from '../../assets/Metro-Boomin-Spider-Man.png'
+import HeaderSocials from "./HeaderSocials"
 
 const Header = () => {
   return (
@@ -12,6 +12,7 @@ const Header = () => {
       <h1>Micheal Hamoud</h1>
       <h5 className="text-light">Fullstack Developer</h5>
       <CTA />
+      <HeaderSocials />
 
       <div className="me">
         <img src={ME} alt="me" />
