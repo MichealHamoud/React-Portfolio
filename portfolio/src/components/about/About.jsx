33baba9f7f71cb864dec637b50ca492f24fb4bb8
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-13 17:58:11 +1000

    addaed about me

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/Metro-Boomin-Spider-Man.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt='About Image'/>
+        </div>
+        </div>
+        <div className="about__content">
+        <div className="about__cards">
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>Still Learning</small>
+          </article>
+
+          <article className='about__card'>
+            <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small>3 local</small>
+          </article>
+
+          <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>20+ Completed</small>
+          </article>
+        </div>
+
+        <p>
+          Hi, My name is Micheal Hamoud. I am a junior Software developer. 
+        </p>
+        <a href="#contact"className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
