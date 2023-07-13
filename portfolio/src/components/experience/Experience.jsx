9f0773c6946051dcaa5e516193d06508b78951a1
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-13 20:11:07 +1000

    added service

--- portfolio/src/components/experience/Experience.jsx
+++ portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,100 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
+import {} from 'react-icons/'
+import {} from 'react-icons/'
+import {} from 'react-icons/'
 
 const Experience = () => {
   return (
-    <section id='experenec'>Experience</section>
+    <section id='experenec'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Bootstraped</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Amature</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>NoSql</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>NodeJs</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Insomia</h4>
+                <small className='text-light'>Middlevel</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
+
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="containter services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX</h3>
+          </div>
+          <ul className='service__list'>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Wireframing</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>User testing</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Cross-Browser Compatibility</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Too be added</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Too be added</p>
+          </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Wireframing</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>User testing</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Cross-Browser Compatibility</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Too be added</p>
+          </li>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Too be added</p>
+          </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+      </div>
+    </section>
   )
 }
 
